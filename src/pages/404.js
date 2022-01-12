7cf771ab539db8e187c8b723a8ea4d4be78039ac
Mike Moore <tigerhollowpoint@gmail.com>
2022-01-12 09:55:11 -0500

    made a 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,48 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link , graphql,useStaticQuery} from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
+import Layout from "../components/Layout"
+import {copyArea,readingSection} from "../components/CopyArea/styles.module.scss"
 // markup
 const NotFoundPage = () => {
+  const data = useStaticQuery(
+    graphql`
+    query {
+      mainMenu: wpMenu(name: {eq: "Main Menu"}) {
+        menuItems {
+          nodes {
+            url
+            label
+            id
+          }
+        }
+      }
+
+    }
+
+    `
+  )
+  const {nodes} = data.mainMenu.menuItems;
+  console.log(nodes);
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    
+   <Layout pageTitle={"Page not found"}>
+     
+     <div className={`${copyArea} ${readingSection}`}>
+       <h1>Page not found</h1>
+       <div style={{marginBottom: "1em"}}>It seems like the page you wanted doesn't exist. Trying looking for something in one of the places below:</div>
+      
+        <ul>
+          {
+            nodes.map((e) => <li style={{marginBottom: ".5em"}}><Link to={e.url}>{e.label}</Link></li>)
+          }
+        </ul>
+      
+     </div>
+     
+   </Layout> 
   )
 }
 
 export default NotFoundPage
+  
