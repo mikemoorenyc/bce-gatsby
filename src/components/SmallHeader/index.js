a0797c8bcb98a323188d4b2060fbe8564c3fe656
Mike Moore <tigerhollowpoint@gmail.com>
2022-02-10 14:14:10 -0500

    Update index.js

--- src/components/SmallHeader/index.js
+++ src/components/SmallHeader/index.js
@@ -2,12 +2,24 @@ import {
     fontSans,
     typeSmaller
 } from "../../global-styles/utilities.module.scss";
-
+import PropTypes from "prop-types"
 import React from "react"
 import {
     header
 } from "./styles.module.scss";
 
-export default function SmallHeader({size, copy, extraClasses}) {
-    return React.createElement(`h${size || "2"}`, {className: `${header} ${fontSans} ${typeSmaller} ${extraClasses || ""}`}, copy);
+const SmallHeader = ({size, copy, extraClasses}) => {
+    return React.createElement(`h${size.toString()}`, {className: `${header} ${fontSans} ${typeSmaller} ${extraClasses}`}, copy);
 }
+
+SmallHeader.defaultProps = {
+ size: 2,
+ extraClasses: ""
+}
+
+SmallHeader.propTypes = {
+ size: PropTypes.number,   
+ copy: PropTypes.string.isRequired,
+ extraClasses : PropTypes.string
+}
+export default SmallHeader
