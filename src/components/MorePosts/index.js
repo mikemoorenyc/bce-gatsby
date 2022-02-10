41e849c8980c2c753549d8cabb0ae0401301586d
Mike Moore <tigerhollowpoint@gmail.com>
2022-02-10 11:36:16 -0500

    Update index.js

--- src/components/MorePosts/index.js
+++ src/components/MorePosts/index.js
@@ -3,19 +3,15 @@ import {
     slimCard,
     postItemsContainer
 } from "./styles.module.scss";
-
+import PropTypes from "prop-types";
 import Card from "../Card";
 import React from "react";
 import SmallHeader from "../SmallHeader";
 
 
-export default function MorePosts({posts,title}) {
- 
-    if(!posts.length) {
-        return null;
-    }
+const MorePosts = ({posts,title}) => {
     return <div className={` ${mpContainer} `}>
-        <SmallHeader copy={title || "More posts"} />
+        <SmallHeader copy={title} />
         <div className={postItemsContainer}>
         {posts.map(e=> {
             e.desc = e.excerpt
@@ -25,3 +21,19 @@ export default function MorePosts({posts,title}) {
 
     </div>
 }  
+MorePosts.defaultProps = {
+    title: "More Posts"
+}
+MorePosts.propTypes = {
+    title: PropTypes.string
+    posts : PropTypes.arrayOf(
+        PropTypes.shape(
+            slug: PropTypes.string,.isRequired
+            desc : PropTypes.string,
+            excerpt: PropTypes.string,
+            kicker: PropTypes.string
+        )
+    ).isRequired
+}
+
+export default MorePosts
