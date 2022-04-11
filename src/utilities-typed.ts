967c89220e17e5de8977d37042c58b972cd070ab
Mike Moore <tigerhollowpoint@gmail.com>
2022-04-11 16:29:13 -0400

    Update utilities-typed.ts

--- src/utilities-typed.ts
+++ src/utilities-typed.ts
@@ -15,5 +15,23 @@ const truncateString = function(str: string, num: number) : string {
 const arraySplit = function(string:string, splitValue : any = /\r?\n/) {
     return string.split(splitValue);
 }
+const inView = function(el,updateCallback) {
+    const element = el ;
+    const interSector = new IntersectionObserver((changes) => {
+        let goodToChange = changes.find(e => e.isIntersecting && e.target == element)
+        if(!goodToChange) {
+            return false; 
+        }
+        updateCallback(); 
+        disconnect(); 
+    }); 
+    const disconnect() => {
+        interSector.disconnect(); 
+    }
+    return {
+        disconnect: disconnect(); 
+    }
+    
+}
 
-export {arraySplit, HtmlStrip,truncateString}
+export {inView, arraySplit, HtmlStrip,truncateString}
