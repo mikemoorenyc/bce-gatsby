8dc2046e6c4eafdcf9ba3112427286dbcf508593
Mike Moore <tigerhollowpoint@gmail.com>
2022-04-19 13:56:06 -0400

    Update DarkModeContext.tsx
    update for dark mode

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { graphql, useStaticQuery} from "gatsby";
+
 
 type DarkModeType = {
     color: undefined | string, 
     colors: string[],
-    getColors: Function,
     darkMode: undefined|boolean,
-    modeSwitch: Function
+    modeSwitch: Function,
+    colorModeInit:Function
 }
 const defaultState : DarkModeType = {
     color:undefined,
     colors: [],
     darkMode: undefined,
     modeSwitch: () => {},
-    getColors: () => {}
+    colorModeInit: () => {}
 }
 const DarkModeContext = React.createContext(defaultState);
 
@@ -22,19 +22,13 @@ const DarkModeContext = React.createContext(defaultState);
 class DarkModeProvider extends React.Component {
     state = {
         color:undefined,
-        colors:[],
+        colors:(process.env.GATSBY_COLORS || "red,black" ).split(","),
         darkMode: undefined
     }
     colorPicker = () : string => {
         return this.state.colors[Math.floor(Math.random() * this.state.colors.length)]
     }
-    getColors = (colorString:string) => {
-        this.setState({
-            colors: (colorString || "darkRed black").split(" ")
-        },() => {
-            this.colorModeInit();
-        })
-    }
+
     modeSwitch = () => {
         if(this.state.darkMode) {
             this.updateColorMode(this.colorPicker())
@@ -50,7 +44,7 @@ class DarkModeProvider extends React.Component {
         localStorage.setItem("dark_mode", dm ? "yes" : "no" );
         
     }
-    colorModeInit() {
+    colorModeInit = () => {
         let dm = localStorage.getItem("dark_mode");
         if(dm === "yes" || (!dm && (window.matchMedia && 
             window.matchMedia('(prefers-color-scheme: dark)').matches)) ) {
@@ -59,13 +53,14 @@ class DarkModeProvider extends React.Component {
         }
         this.updateColorMode(this.colorPicker());
     }
+    
     render() {
         
         return <DarkModeContext.Provider value= {{
             ...this.state,
             ...{
                 modeSwitch:this.modeSwitch,
-                getColors:this.getColors}
+                colorModeInit: this.colorModeInit}
    
         }} >
             {this.props.children}
