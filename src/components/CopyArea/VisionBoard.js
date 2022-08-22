0034f3ee526b3185fec22a4e6299611b4d0560ed
Mike Moore <tigerhollowpoint@gmail.com>
2022-08-22 14:03:50 -0400

    basically nothing

--- src/components/CopyArea/VisionBoard.js
+++ src/components/CopyArea/VisionBoard.js
@@ -1,6 +1,6 @@
 import React from "react"
-import {domToReact} from "html-react-parser";
-import SVG from "../SVG"
+import {domToReact, DOMNode} from "html-react-parser";
+import Svg from "../SVG"
 import {visionBoardStyle} from "./styles.module.scss";
 
 const sections = [
@@ -10,17 +10,16 @@ const sections = [
     ["Product","robot"],
     ["Business Goals", "flag"]
 ]
-const Section = ({secnumb, content, spanner,width="25%"}) =>{
+const Section = ({secnumb, content}) =>{
     const s = sections[secnumb];
     return <div>
-        <h4><SVG icon={s[1]} /><span>{s[0]}</span></h4>
+        <h4><Svg icon={s[1]} /><span>{s[0]}</span></h4>
         {content}
         </div>
 } 
 
 const VisionBoard = ({node}) => {
     let {children} = node
-    
     return <table className={visionBoardStyle}>
         <tr>
            <td colSpan="4"> <Section secnumb={0} content={domToReact(children[0].children)} spanner={4} width="auto"/> </td>
