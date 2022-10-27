19b601c53bc648c05788453c05abb7e251fbd78b
Mike Moore <tigerhollowpoint@gmail.com>
2022-10-27 10:02:56 -0400

    remove php comment

--- src/api/get-tweet.js
+++ src/api/get-tweet.js
@@ -23,37 +23,3 @@ export default async function formHandler(req, res) {
     return res.send(tweet);
 }
  
-
-/*
-<?php
-header("Access-Control-Allow-Origin: *");
-
-
-
-require_once("twitterexchange.php"); 
-$access_token = ""
-$access_token_secret = ""
-$consumer_key = ""
-$consumer_secret = ""
-
-$settings = array(
-    'oauth_access_token' => $access_token,
-    'oauth_access_token_secret' => $access_token_secret,
-    'consumer_key' => $consumer_key,
-    'consumer_secret' => $consumer_secret
-);
-$request = "https://api.twitter.com/1.1/statuses/show.json";
-$method = "GET";
-
-$get_fields = "?id=".$_GET['id'];
-
-$twitter = new TwitterAPIExchange($settings);
-$response = $twitter->setGetfield($get_fields)
-                             ->buildOauth($request, $method)
-                             ->performRequest();
-							 
-echo $response; 
-
-die(); 
-?>
-*/
